0900554d92054076341a3ee388e2fcf4e8bbdd29
Kanika Makhija <kanikamakhija@Kanikas-MacBook-Pro.local>
2020-07-21 20:08:52 -0400

    Time: O(n) and Space: O(1)

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        
+        while(fastPointer!=null && fastPointer.next!=null){
+            
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+            
+            if(fastPointer==slowPointer){
+                return true;
+            }    
+            
+        }
+        return false;
+        
+    }
+}
