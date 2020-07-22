6620b1bfcd26f44f0c8aeded36b60a89b75350a3
Kanika Makhija <kanikamakhija@Kanikas-MacBook-Pro.local>
2020-07-21 22:29:59 -0400

    Time: O(n) and Space: O(n)

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(String token : tokens){
+            
+            if("+-*/".contains(token)){
+                
+                int y = stack.pop();
+                int x = stack.pop();
+                
+                if(token.equals("+"))
+                    stack.push(x+y);
+                
+                else if(token.equals("-"))
+                    stack.push(x-y);
+                
+                else if(token.equals("*"))
+                    stack.push(x*y);
+                
+                else if(token.equals("/"))
+                    stack.push(x/y);
+            }
+                   
+        else{
+            stack.push(Integer.parseInt(token));
+        }
+    }
+        return stack.pop();
+    }
+}
