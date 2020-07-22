9d0af9734fb99b833ecdc8e32c5676cfb1650e5b
Kanika Makhija <kanikamakhija@Kanikas-MacBook-Pro.local>
2020-07-21 21:43:37 -0400

    Time: O(n) and Space: O(n)

---
+++ Trees/UnivaluedBT.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        
+        boolean leftUnivalue = (root.left == null || (root.val == root.left.val && isUnivalTree(root.left)));
+        
+        boolean rightUnivalue = (root.right == null || (root.val == root.right.val && isUnivalTree(root.right)));
+        
+        return leftUnivalue && rightUnivalue;
+        
+    }
+}
